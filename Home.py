c2b97b770f212ee622f184d962f811d4400f327b
anvi-gupta08 <anvi08gupta@gmail.com>
2025-03-22 15:02:26 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -136,7 +136,7 @@ st.markdown(
 st.markdown('<div class="main-content">', unsafe_allow_html=True)
 
 # Adjusted column widths to make the text box wider
-col1, col2 , col3 = st.columns([1, 2, 1])  # Middle column (col2) is now 4, making it wider
+col1, col2 , col3 = st.columns([1, 2.5, 1])  # Middle column (col2) is now 4, making it wider
 
 with col1:
     st.image('logo.png', width=250)
